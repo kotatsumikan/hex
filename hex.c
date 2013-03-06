1f89172b6163988f0a47f305fa864b36d9e5d9fb
kotatsumikan <kotatsumikan@gmail.com>
2013-03-06 20:02:52 +0900

    引数に - を渡すことで標準入力に対応。ファイルオープンエラーをキャッチするように変更

--- hex.c
+++ hex.c
@@ -8,16 +8,24 @@ int main(int argc, char** args){
     FILE *f;
     void *buf;
     if(argc <= 1){
-        printf("Usage: hex filename\n");
+        printf("Usage: hex [FILE|-]\n");
         return(1);
     }
 
     buf = malloc(BUFSIZE);
     buf = memset(buf, 0, BUFSIZE);
 
-    f = fopen(args[1], "r");
+    if(strlen(args[1]) == 1 && *args[1] == '-'){
+        f = stdin;
+    }else{
+        f = fopen(args[1], "r");
+    }
+    if(!f){
+        fprintf(stderr, "file open error\n");
+        return(-1);
+    }
     while(fread(buf, BUFSIZE, 1, f)){
-        printf("%02x", *(char*)buf);
+        printf("%02x", *(unsigned char*)buf);
     }
     fclose(f);
     free(buf);
